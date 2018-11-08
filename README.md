bc0fe4c9dc807813ae401f720c4facdf6333ba46
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2018-11-08 20:45:28 +0100

    improved context

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ Das Labor basiert auf **Python** und setzt (ab Aufgabe 2) eine **Redis** Install
 
 #### 2.2.1. Python
 
-Die Beispiele und Aufgaben sind weitgehend in Python 3 (~3.7) geschrieben.
+Die Beispiele und Aufgaben sind weitgehend in Python 3 (~3.6) geschrieben.
 
 [Python](https://de.wikipedia.org/wiki/Python_(Programmiersprache)) ist eine pragmatische und weit verbreitete objektorientierte (u.a.) Skriptsprache mit dynamischer Typisierung.
 
--- lab2/channel/context.py
+++ lab2/channel/context.py
@@ -1,10 +1,19 @@
 import os
 import sys
 
+
+def add_parent_path(steps_up=1):
+    # construct path by stepping up the path hierarchy <steps_up> times
+    path = os.path.dirname(__file__)
+    for i in range(steps_up):
+        path = os.path.join(path, '..')
+    # add the path to the system search path
+    sys.path.insert(0, path)
+
+
 # Add the toplevel folder of the repository to the module search path
 # This way we can import modules from the shared lib package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), '..'), '..')))
+add_parent_path(2)
 
 # following imports are used by other modules to access shared packages
-from lib import lab_channel
-from lib import lab_logging
+from lib import lab_logging, lab_channel
