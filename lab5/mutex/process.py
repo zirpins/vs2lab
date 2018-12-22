89e9311965c1bf750297573b588a45b3c75a2f43
Christian Zirpins <christian.zirpins@hs-karlsruhe.de>
2018-12-22 18:58:53 +0100

    minor refactoring, log improved for humans

--- lab5/mutex/process.py
+++ lab5/mutex/process.py
@@ -2,7 +2,7 @@ import logging
 import random
 import time
 
-import constMutex
+from constMutex import ENTER, RELEASE, ALLOW
 
 
 class Process:
@@ -41,101 +41,118 @@ class Process:
         self.clock = 0  # The current logical clock
         self.logger = logging.getLogger("vs2lab.lab5.mutex.process.Process")
 
+    def __mapid(self, id='-1'):
+        # resolve channel member address to a human friendly identifier
+        if id == '-1':
+            id = self.process_id
+        return 'Proc_' + chr(65 + self.all_processes.index(id))
+
     def __cleanup_queue(self):
         if len(self.queue) > 0:
             self.queue.sort()
             # There should never be old ALLOW messages at the head of the queue
-            while self.queue[0][2] == constMutex.ALLOW:
+            while self.queue[0][2] == ALLOW:
                 del (self.queue[0])
                 if len(self.queue) == 0:
                     break
 
     def __request_to_enter(self):
         self.clock = self.clock + 1  # Increment clock value
-        # Append request to queue
-        self.queue.append((self.clock, self.process_id, constMutex.ENTER))
+        request_msg = (self.clock, self.process_id, ENTER)
+        self.queue.append(request_msg)  # Append request to queue
         self.__cleanup_queue()  # Sort the queue
-        self.channel.send_to(self.other_processes, (self.clock,
-                             self.process_id, constMutex.ENTER))  # Send request
+        self.channel.send_to(self.other_processes, request_msg)  # Send request
 
     def __allow_to_enter(self, requester):
         self.clock = self.clock + 1  # Increment clock value
-        self.channel.send_to(
-            [requester], (self.clock, self.process_id, constMutex.ALLOW))  # Permit other
+        msg = (self.clock, self.process_id, ALLOW)
+        self.channel.send_to([requester], msg)  # Permit other
 
     def __release(self):
         # need to be first in queue to issue a release
-        assert self.queue[0][1] == self.process_id
+        assert self.queue[0][1] == self.process_id, 'State error: inconsistent local RELEASE'
+
         # construct new queue from later ENTER requests (removing all ALLOWS)
-        tmp = [r for r in self.queue[1:] if r[2] == constMutex.ENTER]
+        tmp = [r for r in self.queue[1:] if r[2] == ENTER]
         self.queue = tmp  # and copy to new queue
         self.clock = self.clock + 1  # Increment clock value
-        self.channel.send_to(self.other_processes, (self.clock,
-                             self.process_id, constMutex.RELEASE))  # Release
+        msg = (self.clock, self.process_id, RELEASE)
+        # Mulicast release notofication
+        self.channel.send_to(self.other_processes, msg)
 
     def __allowed_to_enter(self):
-        processes_with_later_message = set(
-            [req[1] for req in self.queue[1:]])  # See who has sent a message
+         # See who has sent a message
+        processes_with_later_message = set([req[1] for req in self.queue[1:]])
         # Access granted if this process is first in queue and all others have answered (logically) later
-        return self.queue[0][1] == self.process_id and len(self.other_processes) == len(processes_with_later_message)
+        first_in_queue = self.queue[0][1] == self.process_id
+        all_have_answered = len(self.other_processes) == len(processes_with_later_message)
+        return first_in_queue and all_have_answered
 
     def __receive(self):
-        msg = self.channel.receive_from(self.other_processes)[1]  # Pick up any message
+         # Pick up any message
+        _receive = self.channel.receive_from(self.other_processes, 10) 
+        if _receive:
+            msg = _receive[1]
 
-        self.clock = max(self.clock, msg[0])  # Adjust clock value...
-        self.clock = self.clock + 1  # ...and increment
+            self.clock = max(self.clock, msg[0])  # Adjust clock value...
+            self.clock = self.clock + 1  # ...and increment
 
-        self.logger.debug("Process {} received {} from {}.".format(
-            self.process_id,
-            "ENTER" if msg[2] == constMutex.ENTER
-                else "ALLOW" if msg[2] == constMutex.ALLOW
-                    else "RELEASE", msg[1]))
+            self.logger.debug("{} received {} from {}.".format(
+                self.__mapid(),
+                "ENTER" if msg[2] == ENTER
+                else "ALLOW" if msg[2] == ALLOW
+                else "RELEASE", self.__mapid(msg[1])))
 
-        if msg[2] == constMutex.ENTER:
-            self.queue.append(msg)  # Append an ENTER request
-            # and unconditionally allow (don't want to access CS oneself)
-            self.__allow_to_enter(msg[1])
-        elif msg[2] == constMutex.ALLOW:
-            self.queue.append(msg)  # Append an ALLOW
-        elif msg[2] == constMutex.RELEASE:
-            # assure release requester indeed has access (his ENTER is first in queue)
-            assert self.queue[0][1] == msg[1] and self.queue[0][2] == constMutex.ENTER
-            del (self.queue[0])  # Just remove first message
+            if msg[2] == ENTER:
+                self.queue.append(msg)  # Append an ENTER request
+                # and unconditionally allow (don't want to access CS oneself)
+                self.__allow_to_enter(msg[1])
+            elif msg[2] == ALLOW:
+                self.queue.append(msg)  # Append an ALLOW
+            elif msg[2] == RELEASE:
+                # assure release requester indeed has access (his ENTER is first in queue)
+                assert self.queue[0][1] == msg[1] and self.queue[0][2] == ENTER, 'State error: inconsistent remote RELEASE'
+                del (self.queue[0])  # Just remove first message
 
-        self.__cleanup_queue()  # Finally sort and cleanup the queue
-        self.logger.debug("Process {} cleaned local queue {}.".format(
-            self.process_id, self.queue))
+            self.__cleanup_queue()  # Finally sort and cleanup the queue
+        else:        
+            self.logger.warning("{} timed out on RECEIVE.".format(self.__mapid()))
 
-    def enter(self):
+    def init(self):
         self.channel.bind(self.process_id)
-        self.logger.debug("Process {} joined channel.".format(self.process_id))
 
         self.all_processes = list(self.channel.subgroup('proc'))
-        self.all_processes.sort()
+        # sort string elements by numerical order
+        self.all_processes.sort(key=lambda x: int(x))
 
         self.other_processes = list(self.channel.subgroup('proc'))
         self.other_processes.remove(self.process_id)
+
+        self.logger.info("Member {} joined channel as {}."
+                         .format(self.process_id, self.__mapid()))
 
     def run(self):
         while True:
             # Enter the critical section if there are more than one processes left
             # and random is true
             if len(self.all_processes) > 1 and \
-                        random.choice([True, False]):
-                self.logger.debug("Process {} wants to ENTER CS at CLOCK {}.".format(self.process_id, self.clock))
+                    random.choice([True, False]):
+                self.logger.debug("{} wants to ENTER CS at CLOCK {}."
+                    .format(self.__mapid(), self.clock))
+
                 self.__request_to_enter()
                 while not self.__allowed_to_enter():
                     self.__receive()
 
                 # Stay in CS for some time ...
-                sleep_time = random.randint(0, 2)
-                self.logger.debug("Process {} enters CS for {} seconds.".format(self.process_id, sleep_time))
-                print(" CS IN  : {}".format(self.process_id.zfill(3)))
-                time.sleep(sleep_time)
-                print(" CS OUT : {}".format(self.process_id.zfill(3)))
-                print()
+                sleep_time = random.randint(0, 2000)
+                self.logger.debug("{} enters CS for {} milliseconds."
+                    .format(self.__mapid(), sleep_time))
+                print(" CS <- {}".format(self.__mapid()))
+                time.sleep(sleep_time/1000)
 
                 # ... then leave CS
+                print(" CS -> {}".format(self.__mapid()))
                 self.__release()
                 continue
 
