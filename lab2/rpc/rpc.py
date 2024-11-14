e84f76842f9237098f8d76a7f421247f5ea0b62f
heckflorian <florianheck99@gmail.com>
2024-11-14 18:05:01 +0000

    A2

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,7 +1,8 @@
 import constRPC
 
 from context import lab_channel
-
+import threading
+import time
 
 class DBList:
     def __init__(self, basic_list):
@@ -13,6 +14,19 @@ class DBList:
 
 
 class Client:
+
+    class Receiver(threading.Thread):
+        def __init__(self, chan, server, callback):
+            threading.Thread.__init__(self)
+            self.chan = chan
+            self.server = server
+            self.callback = callback
+
+        def run(self):
+            msgrcv = self.chan.receive_from(self.server)  # wait for response
+            self.callback(msgrcv[1]) # pass it to caller
+
+
     def __init__(self):
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
@@ -25,13 +39,18 @@ class Client:
     def stop(self):
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, callback):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
         self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        ack = self.chan.receive_from(self.server)
+        if ack[1] == 'ACK':
+            receiver = self.Receiver(self.chan, self.server, callback)
+            receiver.start()
 
+
+
+   
 
 class Server:
     def __init__(self):
@@ -52,7 +71,27 @@ class Server:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
+                    self.chan.send_to({client}, 'ACK')
+                    time.sleep(10)
                     result = self.append(msgrpc[1], msgrpc[2])  # do local call
+                    
                     self.chan.send_to({client}, result)  # return response
                 else:
                     pass  # unsupported request, simply ignore
+    
+    # def callback(self, val, client):
+    #     self.chan.send_to({client}, val)
+
+
+# class AppendAsync(threading.Thread):
+#     def __init__(self, data, dblist, callback, client):
+#         threading.Thread.__init__(self)
+#         self.data = data
+#         self.dblist = dblist
+#         self.callback = callback
+#         self.client = client
+
+#     def run(self):
+        
+#         assert isinstance(self.dblist, DBList)
+#         self.callback(self.dblist.append(self.data), self.client)
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,7 +1,11 @@
 import rpc
 import logging
+import time
 
 from context import lab_logging
+
+def callback_receiver(msg):
+    print("Result: {}".format(msg.value))
 
 lab_logging.setup(stream_level=logging.INFO)
 
@@ -9,8 +13,15 @@ cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+cl.append('bar', base_list, callback_receiver)
 
-print("Result: {}".format(result_list.value))
+x = 1
+while x < 15:
+    print(x)
+    x = x + 1
+    time.sleep(1)
+    
+# print("Result: {}".format(result_list.value))
 
 cl.stop()
+
